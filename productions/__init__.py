14178c8f489d3ea832b3426a085cc6a36ccbc89a
kottajl <kottajl@o2.pl>
2025-12-12 12:28:00 +0100

    feat: add P11

--- productions/__init__.py
+++ productions/__init__.py
@@ -2,5 +2,7 @@
 
 from productions.production_base import Production
 from productions.p0.p0 import P0
+from productions.p11.p11 import P11
+from productions.p12.p12 import P12
 
-__all__ = ['Production', 'P0']
+__all__ = ['Production', 'P0', 'P11', 'P12']
